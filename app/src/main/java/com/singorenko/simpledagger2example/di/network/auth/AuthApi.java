de4032bf44b3b622e1e4fe18d5699047ec453ab7
Singorenko <cbaezenko@gmail.com>
2019-04-29 15:43:50 +0300

    Add performs retrofit calls with RxJava.

--- app/src/main/java/com/singorenko/simpledagger2example/di/network/auth/AuthApi.java
+++ app/src/main/java/com/singorenko/simpledagger2example/di/network/auth/AuthApi.java
@@ -1,11 +1,13 @@
 package com.singorenko.simpledagger2example.di.network.auth;
 
-import okhttp3.ResponseBody;
-import retrofit2.Call;
+import com.singorenko.simpledagger2example.models.User;
+
+import io.reactivex.Flowable;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface AuthApi {
 
-    @GET
-    Call<ResponseBody> getFakeStuff();
+    @GET("users/{id}")
+    Flowable<User> getUser(@Path("id") int id);
 }
--- app/src/main/java/com/singorenko/simpledagger2example/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/singorenko/simpledagger2example/ui/auth/AuthViewModel.java
@@ -3,10 +3,14 @@ package com.singorenko.simpledagger2example.ui.auth;
 import android.util.Log;
 
 import com.singorenko.simpledagger2example.di.network.auth.AuthApi;
+import com.singorenko.simpledagger2example.models.User;
 
 import javax.inject.Inject;
 
 import androidx.lifecycle.ViewModel;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
 
@@ -18,10 +22,28 @@ public class AuthViewModel extends ViewModel {
         this.authApi = authApi;
         Log.d(TAG, "AuthViewModel: viewModel is working...");
 
-        if(this.authApi == null){
-            Log.d(TAG, "AuthViewModel: auth api is NULL");
-        } else {
-            Log.d(TAG, "AuthViewModel: auth api is NOT NULL");
-        }
+        authApi.getUser(1).toObservable()
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<User>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(User user) {
+                    Log.d(TAG, "onNext: "+user.getEmail());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                    Log.e(TAG, "onError: ", e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 }
